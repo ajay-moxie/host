775b9c8345e5edb175a9010c8cd4585324143d6c
Ajay <ajay@moxiedevices.com>
2014-05-11 21:32:53 +0530

    typedef file
    Signed-off-by: Ajay <ajay@moxiedevices.com>

---
+++ typedef.h
@@ -0,0 +1,46 @@
+
+#ifndef	__TYPEDEF_H__
+#define __TYPEDEF_H__
+
+
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed long int32_t;
+typedef unsigned long uint32_t;
+
+/******************************************************************************
+Macro definitions
+******************************************************************************/
+
+/* definition of logical value */
+//#define TRUE				1
+//#define FALSE				0
+
+/* definition of bit setting value */
+//#define	BIT_SET				1
+//#define	BIT_CLR				0
+
+/* definition of port I/O value */
+//#define	LEVEL_LOW			0
+//#define	LEVEL_HIGH			1
+
+/* definition of port mode setting value */
+//#define	INPUT				1
+//#define	OUTPUT				0
+
+/* definition of interrupt mask setting value */
+//#define	INTERRUPT_UNMASKED	0
+//#define	INTERRUPT_MASKED	1
+
+/* definition of flag variables setting value */
+//#define	FLAG_DOWN			0
+//#define	FLAG_UP				1
+
+/* definition of priority setting value */
+//#define	PRIORITY_HIGH			0
+//#define PRIORITY_LOW			1
+
+
+#endif
